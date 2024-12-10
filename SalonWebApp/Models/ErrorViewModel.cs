1328f5c7ca5c242bbd9c8000c3a0d99f95255fd8
omerasaf <balikciomerasaf@gmail.com>
2024-12-11 01:10:33 +0300

    project started

---
+++ SalonWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalonWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
