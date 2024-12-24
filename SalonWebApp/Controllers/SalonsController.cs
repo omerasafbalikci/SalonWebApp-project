383c6847746a146e61a1f209c5bbb3440b992d82
ysn52 <cevaty48@gmail.com>
2024-12-25 01:42:47 +0300

    added SalonsController

---
+++ SalonWebApp/Controllers/SalonsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalonWebApp.Models;
+
+namespace SalonWebApp.Controllers
+{
+    [Authorize]
+    public class SalonsController : Controller
+    {
+        private readonly SalonContext _context;
+
+        public SalonsController(SalonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Salons
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Salons.ToListAsync());
+        }
+
+        // GET: Salons/Details/5
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salons
+                .Include(s => s.Employees)
+                .Include(s => s.Services)
+                .Include(s => s.Appointments)
+                .FirstOrDefaultAsync(m => m.SalonId == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        // GET: Salons/Create
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salons/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Create(Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        // GET: Salons/Edit/5
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salons.FindAsync(id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+            return View(salon);
+        }
+
+        // POST: Salons/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Edit(int id, Salon salon)
+        {
+            if (id != salon.SalonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalonExists(salon.SalonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        // GET: Salons/Delete/5
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salons
+                .FirstOrDefaultAsync(m => m.SalonId == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        // POST: Salons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salon = await _context.Salons.FindAsync(id);
+            if (salon != null)
+            {
+                _context.Salons.Remove(salon);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalonExists(int id)
+        {
+            return _context.Salons.Any(e => e.SalonId == id);
+        }
+    }
+}
--- SalonWebApp/obj/Debug/net8.0/SalonWebApp.AssemblyInfo.cs
+++ SalonWebApp/obj/Debug/net8.0/SalonWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalonWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cbf9ba72fd4e99d867dc03444ebad9a3be074a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c785be599411ccb58b3512a17fe3e8804bf3275")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalonWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalonWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
