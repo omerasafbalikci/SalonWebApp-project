607d2602233b027206825c94453529a61fb58201
ysn52 <cevaty48@gmail.com>
2024-12-25 04:38:01 +0300

    added TimesController

---
+++ SalonWebApp/Controllers/TimesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalonWebApp.Models;
+
+namespace SalonWebApp.Controllers
+{
+    [Authorize(Roles = "ADMIN")]
+    public class TimesController : Controller
+    {
+        private readonly SalonContext _context;
+
+        public TimesController(SalonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Times
+        public async Task<IActionResult> Index()
+        {
+            var times = await _context.Times.Include(t => t.Appointments).ToListAsync();
+            return View(times);
+        }
+
+        // GET: Times/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var time = await _context.Times.Include(t => t.Appointments)
+                .FirstOrDefaultAsync(m => m.TimeId == id);
+            if (time == null)
+            {
+                return NotFound();
+            }
+
+            return View(time);
+        }
+
+        // GET: Times/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Times/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Time time)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(time);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(time);
+        }
+
+        // GET: Times/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var time = await _context.Times.FindAsync(id);
+            if (time == null)
+            {
+                return NotFound();
+            }
+            return View(time);
+        }
+
+        // POST: Times/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Time time)
+        {
+            if (id != time.TimeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(time);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TimeExists(time.TimeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(time);
+        }
+
+        // GET: Times/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var time = await _context.Times.Include(t => t.Appointments)
+                .FirstOrDefaultAsync(m => m.TimeId == id);
+            if (time == null)
+            {
+                return NotFound();
+            }
+
+            return View(time);
+        }
+
+        // POST: Times/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var time = await _context.Times.FindAsync(id);
+            if (time != null)
+            {
+                _context.Times.Remove(time);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TimeExists(int id)
+        {
+            return _context.Times.Any(e => e.TimeId == id);
+        }
+    }
+}
---
+++ SalonWebApp/Controllers/WorkingDaysController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalonWebApp.Models;
+
+namespace SalonWebApp.Controllers
+{
+    [Authorize(Roles = "ADMIN")]
+    public class WorkingDaysController : Controller
+    {
+        private readonly SalonContext _context;
+
+        public WorkingDaysController(SalonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WorkingDays
+        public async Task<IActionResult> Index()
+        {
+            var workingDays = _context.WorkingDays.Include(w => w.Employee);
+            return View(await workingDays.ToListAsync());
+        }
+
+        // GET: WorkingDays/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workingDay = await _context.WorkingDays
+                .Include(w => w.Employee)
+                .FirstOrDefaultAsync(m => m.WorkingDayId == id);
+            if (workingDay == null)
+            {
+                return NotFound();
+            }
+
+            return View(workingDay);
+        }
+
+        // GET: WorkingDays/Create
+        public IActionResult Create()
+        {
+            ViewBag.Employees = _context.Employees.ToList();
+            return View();
+        }
+
+        // POST: WorkingDays/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(WorkingDay workingDay)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(workingDay);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(workingDay);
+        }
+
+        // GET: WorkingDays/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workingDay = await _context.WorkingDays.FindAsync(id);
+            if (workingDay == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Employees = _context.Employees.ToList();
+            return View(workingDay);
+        }
+
+        // POST: WorkingDays/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, WorkingDay workingDay)
+        {
+            if (id != workingDay.WorkingDayId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workingDay);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkingDayExists(workingDay.WorkingDayId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(workingDay);
+        }
+
+        // GET: WorkingDays/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workingDay = await _context.WorkingDays
+                .Include(w => w.Employee)
+                .FirstOrDefaultAsync(m => m.WorkingDayId == id);
+            if (workingDay == null)
+            {
+                return NotFound();
+            }
+
+            return View(workingDay);
+        }
+
+        // POST: WorkingDays/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var workingDay = await _context.WorkingDays.FindAsync(id);
+            if (workingDay != null)
+            {
+                _context.WorkingDays.Remove(workingDay);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkingDayExists(int id)
+        {
+            return _context.WorkingDays.Any(e => e.WorkingDayId == id);
+        }
+    }
+}
